986f2b7f041144188c4ad7cca13eeda958f4a739
Guilherme Ferreira <guifrribeiro@gmail.com>
2020-04-11 10:39:49 -0300

    Get the images

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -12,6 +13,7 @@ mongoose.connect('mongodb+srv://footplayer:footplayer@footplayer-gf8yz.mongodb.n
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
